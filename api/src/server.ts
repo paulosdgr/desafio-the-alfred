db2d4b08304f44c54888b9b0948e165ca8310bbe
Paulo Victor <paulovictor.souza@wakke.co>
2023-08-29 08:35:23 -0300

    feat/api/create-server

---
+++ api/src/server.ts
@@ -0,0 +1,27 @@
+import express, { Express, json } from 'express';
+import { ApolloServer } from '@apollo/server';
+
+import { typeDefs, resolvers } from './graphql';
+import * as dotenv from 'dotenv';
+import { expressMiddleware } from '@apollo/server/express4';
+import http from 'http';
+import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer';
+import cors from 'cors';
+const app: Express = express();
+const httpServer = http.createServer(app);
+
+dotenv.config();
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+});
+
+server.start().then(() => {
+    app.use('/', cors(), json(), expressMiddleware(server));
+});
+
+app.listen(4000, () => {
+    console.log('Server is listening on http://localhost:4000/graphql');
+});
